ff79a3042cdfb2cdd334502d3f5e198d844ef8ef
Василий <vonavud@gmail.com>
2021-07-12 21:34:58 +0300

    fourth step

--- src/Cli.php
+++ src/Cli.php
@@ -1,10 +1,13 @@
 <?php
+
 namespace Php\Project\Lvl1\Cli;
+
 use function cli\line;
 use function cli\prompt;
+
 function Greeting()
 {
-line('Welcome to the Brain Game!');
-$name = prompt('May I have your name?');
-line("Hello, %s!", $name);
+    line('Welcome to the Brain Game!');
+    $name = prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
