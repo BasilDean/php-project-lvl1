8c0837cb28f936f363d453cf780a12f170ba5a72
Василий <vonavud@gmail.com>
2021-07-21 21:48:33 +0300

    corrected some more mistakes

--- src/Engine.php
+++ src/Engine.php
@@ -5,7 +5,7 @@ namespace Php\Project\Lvl1\Engine;
 use function cli\line;
 use function cli\prompt;
 
-function makeGame(string $description, array $questions, array $expectedAnswers)
+function makeGame(string $description, array $questions, array $expectedAnswers): Collection
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
