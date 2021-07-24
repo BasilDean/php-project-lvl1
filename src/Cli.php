fd6b220493a6232b97b6e08021d783ae0186fdde
Василий <vonavud@gmail.com>
2021-07-24 14:33:21 +0300

    cheked cli

--- src/Cli.php
+++ src/Cli.php
@@ -5,10 +5,9 @@ namespace Php\Project\Lvl1\Cli;
 use function cli\line;
 use function cli\prompt;
 
-function Greeting(): Collection
+function Greeting(): void
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, %s!", $name);
-    return $name;
 }
