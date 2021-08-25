e73407c444e1d859d465664799527a23af02499c
Василий <vonavud@gmail.com>
2021-08-25 15:48:21 +0300

    fixed prime4

--- src/Games/Prime.php
+++ src/Games/Prime.php
@@ -8,7 +8,7 @@ use const BrainGames\Engine\ROUND_COUNTER;
 
 const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
-function is_prime($n): bool
+function is_prime(int $n): bool
 {
     if (($n === 1) || ($n === 2)) {
         return true;
